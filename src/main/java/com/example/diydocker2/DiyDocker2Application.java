e2cf1aeac27c2e28d049c417606a9b3505bd4e7c
tobiasrusvik <toru010@EGMS.no>
2024-10-01 10:34:11 +0200

    Docker setup

---
+++ src/main/java/com/example/diydocker2/DiyDocker2Application.java
@@ -0,0 +1,13 @@
+package com.example.diydocker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyDocker2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiyDocker2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diydocker2/DiyDocker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diydocker2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyDocker2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
